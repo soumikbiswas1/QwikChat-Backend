a919c57155c430026a4ca309f5bf30ff690ab1b3
soumikbiswas1 <soumikjiswas@gmail.com>
2023-06-23 13:45:57 +0530

    first commit

---
+++ services/mailer.js
@@ -0,0 +1,40 @@
+
+const sgMail = require("@sendgrid/mail");
+console.log(process.env.SENDGRID_API_KEY)
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+const sendSGMail = async ({
+  to,
+  sender,
+  subject,
+  html,
+  attachments,
+  text,
+}) => {
+  try {
+    const from = "chatterjeeprasun27@gmail.com";
+
+    const msg = {
+      to: to, // Change to your recipient
+      from: from, // Change to your verified sender
+      subject: subject,
+      html: html,
+      // text: text,
+      attachments,
+    };
+
+    
+    return sgMail.send(msg);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+exports.sendEmail = async (args) => {
+  if (!process.env.NODE_ENV === "development") {
+    return Promise.resolve();
+  } else {
+    return sendSGMail(args);
+  }
+};
